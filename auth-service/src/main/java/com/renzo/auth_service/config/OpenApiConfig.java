662916396c6a1f5841001d18e6677fae38fdea16
Renzo <107525824+rnzmndz@users.noreply.github.com>
2025-07-26 20:53:21 +0800

    Add Cloud to servers

--- auth-service/src/main/java/com/renzo/auth_service/config/OpenApiConfig.java
+++ auth-service/src/main/java/com/renzo/auth_service/config/OpenApiConfig.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Configuration
 public class OpenApiConfig {
 
-    @Value("${services.api-gateway.url}"
+    @Value("${services.api-gateway.url}")
     private String cloudUrl;
 
     @Bean
