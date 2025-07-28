d11721d81443d3d4f321d8b71221d3f3e559e907
Renzo <107525824+rnzmndz@users.noreply.github.com>
2025-07-28 14:41:22 +0800

    Fix the servers

--- auth-service/src/main/java/com/renzo/auth_service/config/OpenApiConfig.java
+++ auth-service/src/main/java/com/renzo/auth_service/config/OpenApiConfig.java
@@ -12,30 +12,29 @@ import java.util.List;
 @Configuration
 public class OpenApiConfig {
 
-    @Value("${services.api-gateway.url}")
+    @Value("${services.api-gateway.url:http://localhost:8080}")
     private String cloudUrl;
 
     @Bean
     public OpenAPI customOpenAPI() {
         String ip = NetworkUtils.getLocalIpAddress();
-        
-         Server cloud = new Server()
-                .url(cloudUrl)
-                .description("Cloud");
+
+        Server gateway = new Server()
+                .url(cloudUrl + "/auth-service")
+                .description("API Gateway");
 
         Server localhost = new Server()
-                .url("http://localhost:8080")
-                .description("Localhost");
+                .url("http://localhost:8080/auth-service")
+                .description("Localhost through API Gateway");
 
         Server ipBased = new Server()
-                .url("http://" + ip + ":8080")
-                .description("Current machine IP (" + ip + ")");
+                .url("http://" + ip + ":8080/auth-service")
+                .description("Current machine IP through Gateway");
 
         Server docker = new Server()
-                .url("http://host.docker.internal:8080")
-                .description("Docker Host");
+                .url("http://host.docker.internal:8080/auth-service")
+                .description("Docker Host through Gateway");
 
-        return new OpenAPI()
-                .servers(List.of(localhost, ipBased, docker, cloud));
+        return new OpenAPI().servers(List.of(gateway, localhost, ipBased, docker));
     }
 }
