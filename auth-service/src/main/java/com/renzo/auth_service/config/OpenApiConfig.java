6838d9596cc8fd3ca9ca78ff7cd5f6afac733a03
Renzo <107525824+rnzmndz@users.noreply.github.com>
2025-07-28 14:22:20 +0800

    Add Value import

--- auth-service/src/main/java/com/renzo/auth_service/config/OpenApiConfig.java
+++ auth-service/src/main/java/com/renzo/auth_service/config/OpenApiConfig.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.util.List;
 
