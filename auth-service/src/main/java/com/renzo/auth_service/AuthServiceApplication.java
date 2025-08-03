a4bc3ac54c4b8f9fd3417c32e4586ff2badc158d
Renzo Mendoza <renzomendoza.me@gmail.com>
2025-08-03 16:02:16 +0800

    update

--- auth-service/src/main/java/com/renzo/auth_service/AuthServiceApplication.java
+++ auth-service/src/main/java/com/renzo/auth_service/AuthServiceApplication.java
@@ -3,7 +3,7 @@ package com.renzo.auth_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+//import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 //@EnableJpaAuditing(auditorAwareRef = "auditorAware")
