d9aa66d1f162a1ef61a3d7e12f6982089c94d110
Renzo Mendoza <renzomendoza.me@gmail.com>
2025-08-03 16:00:12 +0800

    update

--- auth-service/src/main/java/com/renzo/auth_service/audit/CustomAuditorAware.java
+++ auth-service/src/main/java/com/renzo/auth_service/audit/CustomAuditorAware.java
@@ -1,15 +1,15 @@
 package com.renzo.auth_service.audit;
 
-import org.springframework.data.domain.AuditorAware;
+//import org.springframework.data.domain.AuditorAware;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
 @Component("auditorAware")
-public class CustomAuditorAware implements AuditorAware<String> {
+public class CustomAuditorAware /* implements AuditorAware<String> */ {
 
-    @Override
-    public Optional<String> getCurrentAuditor() {
-        return UserContext.getUser().or(() -> Optional.of("system"));
-    }
+//    @Override
+//    public Optional<String> getCurrentAuditor() {
+//        return UserContext.getUser().or(() -> Optional.of("system"));
+//    }
 }
