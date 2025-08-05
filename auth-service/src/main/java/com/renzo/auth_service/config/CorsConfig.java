d985bbdbf7ea30400411666e5a1f9afd74223490
Renzo Mendoza <renzomendoza.me@gmail.com>
2025-08-05 20:25:46 +0800

    update

--- auth-service/src/main/java/com/renzo/auth_service/config/CorsConfig.java
+++ auth-service/src/main/java/com/renzo/auth_service/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("http://localhost:4200", "http://localhost:8080")
                         .allowedMethods("*")
                         .allowedHeaders("*");
             }
