c54dd02344b07635e63996d073f408e920d36fbd
Renzo Mendoza <renzomendoza.me@gmail.com>
2025-08-05 16:04:39 +0800

    update

--- api-gateway/src/main/java/com/renzo/api_gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/renzo/api_gateway/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
@@ -19,16 +21,26 @@ public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain filterChain(ServerHttpSecurity http) {
         http
-            .cors(cors -> {}) // Enable CORS
-            .csrf(ServerHttpSecurity.CsrfSpec::disable) // Disable CSRF
-            .authorizeExchange(exchanges -> exchanges
-                .pathMatchers("/public/**", "/auth/**", "/webjars/swagger-ui/**", "/api-docs/**").permitAll()
-                .pathMatchers(HttpMethod.GET, "/employee-service/v3/api-docs").permitAll()
-                .anyExchange().authenticated()
-            )
-            .oauth2ResourceServer(oauth2 -> oauth2
-                .jwt(Customizer.withDefaults())
-            );
+                .cors(cors -> {}) // Enable CORS
+                .csrf(ServerHttpSecurity.CsrfSpec::disable) // Disable CSRF
+                .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers("/public/**", "/auth/**", "/webjars/swagger-ui/**", "/api-docs/**").permitAll()
+                        .pathMatchers(HttpMethod.GET, "/employee-service/v3/api-docs").permitAll()
+                        .anyExchange().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(Customizer.withDefaults())
+                        .authenticationEntryPoint((exchange, ex) -> {
+                            ServerHttpResponse response = exchange.getResponse();
+                            response.setStatusCode(HttpStatus.UNAUTHORIZED);
+
+                            // Add CORS headers even on error
+                            response.getHeaders().add("Access-Control-Allow-Origin", "http://localhost:4200");
+                            response.getHeaders().add("Access-Control-Allow-Credentials", "true");
+
+                            return response.setComplete();
+                        })
+                );
 
         return http.build();
     }
