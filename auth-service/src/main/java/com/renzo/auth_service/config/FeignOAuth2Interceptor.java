c85dd0f4132c7efa0303c2ff0cb0adec21054c4f
Renzo Mendoza <renzomendoza.me@gmail.com>
2025-08-07 17:19:04 +0800

    update

--- auth-service/src/main/java/com/renzo/auth_service/config/FeignOAuth2Interceptor.java
+++ auth-service/src/main/java/com/renzo/auth_service/config/FeignOAuth2Interceptor.java
@@ -19,7 +19,7 @@ public class FeignOAuth2Interceptor implements RequestInterceptor {
     @Override
     public void apply(RequestTemplate template) {
         OAuth2AuthorizeRequest authorizeRequest = OAuth2AuthorizeRequest
-                .withClientRegistrationId("keycloak")
+                .withClientRegistrationId("keycloak-service")
                 .principal("auth-service") // just a string ID
                 .build();
 
