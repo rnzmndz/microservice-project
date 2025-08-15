7910da9cd4cc85ff0a4cacb7f8e25553e89580bf
Renzo Mendoza <renzomendoza.me@gmail.com>
2025-08-15 20:53:21 +0800

    update

--- auth-service/src/main/java/com/renzo/auth_service/config/OpenApiConfig.java
+++ auth-service/src/main/java/com/renzo/auth_service/config/OpenApiConfig.java
@@ -1,6 +1,8 @@
 package com.renzo.auth_service.config;
 
 import com.renzo.auth_service.utils.NetworkUtils;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +12,12 @@ import org.springframework.beans.factory.annotation.Value;
 import java.util.List;
 
  @Configuration
+ @SecurityScheme(
+         name = "bearerAuth",
+         type = SecuritySchemeType.HTTP,
+         bearerFormat = "JWT",
+         scheme = "bearer"
+ )
  public class OpenApiConfig {
 
      @Value("${services.api-gateway.url:http://api-gateway:8080}")
