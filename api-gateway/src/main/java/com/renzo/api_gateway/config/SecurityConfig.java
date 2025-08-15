2655c8f762dbd20cacdc2f4064be6881d192dd90
Renzo Mendoza <renzomendoza.me@gmail.com>
2025-08-15 21:30:39 +0800

    update

--- api-gateway/src/main/java/com/renzo/api_gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/renzo/api_gateway/config/SecurityConfig.java
@@ -2,19 +2,15 @@ package com.renzo.api_gateway.config;
 
 import com.renzo.api_gateway.service.CookieAuthenticationSuccessHandler;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.security.oauth2.jwt.ReactiveJwtDecoders;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.web.server.ServerWebExchange;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -57,30 +53,5 @@ public class SecurityConfig {
     public ReactiveJwtDecoder jwtDecoder() {
         return ReactiveJwtDecoders.fromIssuerLocation(jwtIssuerUri);
     }
-
-    @Bean
-    public GlobalFilter forwardUserClaimsFilter() {
-        return (exchange, chain) -> {
-            String path = exchange.getRequest().getURI().getPath();
-            if (path.startsWith("/v3/api-docs") || path.startsWith("/swagger-ui") || path.startsWith("/webjars/swagger-ui")) {
-                return chain.filter(exchange);
-            }
-
-            return exchange.getPrincipal()
-                    .flatMap(principal -> {
-                        if (principal instanceof JwtAuthenticationToken jwtAuth) {
-                            Jwt jwt = jwtAuth.getToken();
-                            ServerWebExchange mutatedExchange = exchange.mutate().request(r -> r.headers(headers -> {
-                                headers.add("X-User-Id", jwt.getClaimAsString("sub"));
-                                headers.add("X-User-Roles", String.join(",", jwt.getClaimAsStringList("roles")));
-                            })).build();
-                            return chain.filter(mutatedExchange);
-                        }
-                        return chain.filter(exchange);
-                    });
-        };
-    }
-
-
 
 }
