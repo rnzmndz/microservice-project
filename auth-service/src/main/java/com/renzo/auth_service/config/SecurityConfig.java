8838a62a772408dcdb1ea4a0574679484cec5d27
Renzo Mendoza <renzomendoza.me@gmail.com>
2025-08-17 18:04:30 +0800

    update

--- auth-service/src/main/java/com/renzo/auth_service/config/SecurityConfig.java
+++ auth-service/src/main/java/com/renzo/auth_service/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.renzo.auth_service.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -24,6 +25,9 @@ public class SecurityConfig {
                                 "/auth/**"
                         ).permitAll()
                         .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(Customizer.withDefaults())
                 );
         return http.build();
     }
