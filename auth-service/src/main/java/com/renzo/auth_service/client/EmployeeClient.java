6d7f509a5f6e4e4538af51f42d0baaf99d2abad3
Renzo Mendoza <renzomendoza.me@gmail.com>
2025-08-31 17:48:22 +0800

    remove the url in OpenFeign

--- auth-service/src/main/java/com/renzo/auth_service/client/EmployeeClient.java
+++ auth-service/src/main/java/com/renzo/auth_service/client/EmployeeClient.java
@@ -7,8 +7,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "employee-service",
-            url="${services.employee-service.url:http://employee-service:8082}")
+@FeignClient(name = "employee-service")
 public interface EmployeeClient {
 
     @PostMapping("/api/v1/employees")
